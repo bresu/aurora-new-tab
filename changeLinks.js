e6ca87cd767491ab01c30b0f4cce55e6144e7b87
bresu <63677996+bresu@users.noreply.github.com>
2021-10-29 12:27:56 +0200

    deletion

--- changeLinks.js
+++
@@ -1,5 +0,0 @@
-var links = document.links;
-for (var i = 0; i < links.length; i++) {
-     links[i].target = "_blank";
-}
-// shout-out an stack overflow! und warum liest du das hier? kannst mir auch gleich auf github folgen: https://github.com/bresu
